67caeeb279064d96c5a4b0e1bb0f001e4a558b8a
Jordy Fontoura <jordyfontoura@outlook.com>
2021-07-07 15:31:46 -0300

    🌱 Seed implementada

--- assets/index.ts
+++ assets/index.ts
@@ -1,17 +1,38 @@
 import { Cenário, GameEngine, IGameConfig } from "game/core";
+import seed from "seed-random";
 import cenários from "./scenes";
-interface IAssets{
+
+interface IAssets {
   cenários: Cenário[];
   carregar(jogo: GameEngine): void;
-  configurações?: IGameConfig
+  configurações?: IGameConfig;
 }
+const seedsAceitas =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
+
+function randomSeed() {
+  let res = "";
+  while (res.length < 5 + Math.random() * 25) {
+    res += seedsAceitas[Math.floor(Math.random() * seedsAceitas.length)];
+  }
+  return res;
+}
+const url = new URL(window.location.href);
+let useSeed = randomSeed();
+let urlSeed = url.pathname.split("/").pop();
+if (urlSeed) {
+  if (!["play"].includes(urlSeed)) {
+    useSeed = urlSeed;
+  }
+}
+console.log("Seed:", useSeed);
+seed(useSeed, { global: true });
+
 const Assets: IAssets = {
   cenários,
-  carregar(){
-    
-  },
+  carregar() {},
   configurações: {
-    fps: 60
-  }
+    fps: 240,
+  },
 };
 export default Assets;
